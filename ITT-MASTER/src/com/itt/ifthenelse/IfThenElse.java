bf39fad35cc7297da8fda2214c2672c48bf0df93
student <student@MININT-A9P576U>
2015-06-22 20:13:05 -0700

    test updating if then else

---
+++ ITT-MASTER/src/com/itt/ifthenelse/IfThenElse.java
@@ -0,0 +1,17 @@
+package com.itt.ifthenelse;
+
+public class IfThenElse {
+	
+	static int x = 10 + 5 * 2;
+	
+	public static void main(String[] args) {
+		
+		if (x < 20) {
+			System.out.println("X is less than 20!");
+		} else {
+			System.out.println("X is greater than 20!");
+		}
+
+	}
+
+}
